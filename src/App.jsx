cb9c9ae86f3b3eafc191f170e10292c02441cfc9
rooomaisa <romyvandam334@gmail.com>
2024-09-06 12:25:12 +0200

    finished 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,33 @@
 import './App.css'
+import Button from "./components/Button.jsx";
+
 
 function App() {
-  return (
+  return ( <>
       <h1>Handbags & Purses</h1>
+
+        <nav>
+            <Button text="to the collection"
+                    disabled={false}/>
+            <Button text="shop all bags"
+                    disabled={false}/>
+            <Button text="pre-orders"
+                    disabled={true}/>
+        </nav>
+
+          <main>
+              <article>
+                  <span>red lable </span>
+                  <img src="image" alt="description" />
+                  <p>Title</p>
+                  <h4>Price</h4>
+              </article>
+
+
+          </main>
+
+
+      </>
   )
 }
 
